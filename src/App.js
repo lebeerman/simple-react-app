32fc2f1a480d03c7c80a514b661327f4a2f92d2d
lebeerman <omnidbeerman@gmail.com>
2018-01-22 13:14:24 -0700

    updated after breakout. running on compile.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import CharacterList from './components/CharacterList'
 
 class App extends Component {
-  state = {characters: []}
+  state = {
+    characters: [],
+    count: 0
+  }
 
   componentDidMount() {
-    console.log('mounting')
+    console.log('Mounting!');
+    this.getPeople();
+  }
+
+  deletePeople = name => {
+    let characters = this.state.characters
+    let idx = characters.findIndex(char => char.name === name)
+    characters.splice(idx, 1)
+  }
+
+  getPeople = () => {
+    console.log('getPeople()')
+    let count = this.state.count
+
     fetch('http://galvanize-cors-proxy.herokuapp.com/https://swapi.co/api/people')
       .then(response => response.json())
       .then(data => {
-        this.setState({characters: data.results})
-        // console.log(this.state)
+        this.setState({characters: data.results, count: ++count})
+        console.log('getPeople() result=', this.state)
       })
   }
 
@@ -26,7 +42,11 @@ class App extends Component {
         <p className="App-intro">
           Here's a list of students in Galvanize!
         </p>
-        <CharacterList characters={this.state.characters}/>
+        <CharacterList 
+          deletePeople={this.state.deletePeople}
+          characters={this.state.characters} 
+          getPeople={this.state.getPeople}
+          count={this.state.count} />
       </div>
     );
   }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,20 @@
+/* ESLint: jsx */
 import React from 'react';
 import CharacterCard from './CharacterCard'
+// Front end frameworks: data down -- actions up 
 
-const CharacterList = (props) => {
-  const characters = props.characters
-  console.log('characters', characters)
+const CharacterList = ({characters = [], count = 0, deletePeople, getPeople}) => {
+  // const characters = props.characters
+  // const getFart = props.getPeople
+  // const count = props.count || 0
 
-  return characters.map((character, index) => {
-   return <CharacterCard key={character.name} character={character} rank={index} />
-  })
+  return <div> 
+    <button onClick={getPeople}>Refresh</button>
+    <legend>Count: {count}</legend>
+    {characters.map((character, index) => {
+      return <CharacterCard key={character.name} deletePeople={deletePeople} character={character} rank={index} />
+    })}
+  </div>
 }
 
 
